d46ea4b2952caec30fc66ca71b8e17825e6e5ea9
Manuel Coppo <manuelcoppo@hotmail.fr>
2022-06-26 16:20:35 +0200

    make color picker githubPicker

--- client/src/components/StartingForm.js
+++ client/src/components/StartingForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { CirclePicker, GithubPicker, SketchPicker } from 'react-color';
+import { GithubPicker } from 'react-color';
 
 class StartingForm extends Component{
 
