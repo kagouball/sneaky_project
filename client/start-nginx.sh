4cff73e80e17862ce325664e3363964ee3617a40
Rachyd Amine <58592778+aminerachyd@users.noreply.github.com>
2023-02-13 11:43:03 +0100

    Update start-nginx.sh

--- client/start-nginx.sh
+++ client/start-nginx.sh
@@ -2,7 +2,6 @@
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 for file in $JSFOLDER;
 do
-  #cat $file | envsubst $EXISTING_VARS | tee /usr/share/nginx/html/static/js/$(basename $file)
   envsubst $EXISTING_VARS < $file | sponge $file
 done
 nginx -g 'daemon off;'
